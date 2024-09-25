1f7c67899f419db8814862ba770e8b8a9b6f83f1
Mihail Denisenko <i@mihail-denisenko.ru>
2024-09-25 18:05:08 +0300

    next-commit-25.09.2024-18:05

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+var btn_more = document.querySelector('.more');
+var wrapper = document.querySelector('.swiper-wrapper');
 
 
 
@@ -5,12 +7,11 @@ window.addEventListener('DOMContentLoaded', () => {
   var wid = document.querySelector('.body').clientWidth
   
  
-  if (wid > 319 && wid < 768) {
+  if (wid > 319 && wid < 500) {
     console.log(`Новые размеры: ${wid}`);
-    const mySwiper = new Swiper(".swiper", {
+    var swiper = new Swiper(".swiper", {
 
       slidesPerView: "auto",
-      spaceBetween: 0,
       freeMode: true,
       
       pagination: {
@@ -24,3 +25,17 @@ window.addEventListener('DOMContentLoaded', () => {
     
   }
 });
+
+btn_more.addEventListener('click', () => {
+
+  // el[0].style = 'overflow-y: visible';
+
+  if (wrapper.classList.contains('wrapper-start') === false) {
+    wrapper.classList.add('wrapper-start')
+    wrapper.classList.remove('wrapper-end')
+  } else {
+    wrapper.classList.add('wrapper-end')
+    wrapper.classList.remove('wrapper-start')
+  }
+})
+
