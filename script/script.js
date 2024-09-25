d63c628a2af548d31c6dfc908484e56077db98f4
Mihail Denisenko <i@mihail-denisenko.ru>
2024-09-25 13:50:58 +0300

    new_commit_from25.09.2024_13:50

--- script/script.js
+++ script/script.js
@@ -1,54 +1,39 @@
-const swiper_h = new Swiper('.swiper-h', {
-  // Optional parameters
-  // direction: 'vertical',
-  direction: 'horizontal',
-  autoplay: true,
-  loop: true,
-  speed: 600,
 
-  // freeMode: true, 
+
+
+window.addEventListener('resize', () => {
+  var wid = document.querySelector('.mySwiper').clientWidth
   
-  slidesPerView: 'auto',
+ 
+  if (wid > 320 && wid < 768) {
+    console.log(`Новые размеры: ${wid}`);
+    // swiper = ''
+  
+    var swiper = new Swiper(".mySwiper", {
 
-  // effect: 'cube',
-  // cubeEffect: {
-  //   slideShadows: false,
-  // },
-  //   effect: 'flip',
-  // flipEffect: {
-  //   slideShadows: false,
-  // },
-  // effect: 'cards',
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+      
+      
+      
+      
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        dynamicBullets: true,
+        clickable: true,
+      },
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    dynamicBullets: true,
-    clickable: true,
-  },
+      // Navigation arrows
+      //   navigation: {
+      //   nextEl: '.swiper-button-next',
+      //   prevEl: '.swiper-button-prev',
+      // },
 
-  // Navigation arrows
-    navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
-});
-
-
-const swiper_v = new Swiper('.swiper-v', {
-  direction: 'vertical',
-
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    dynamicBullets: true,
-    clickable: true,
-  },
+      // And if we need scrollbar
+      // scrollbar: {
+      //   el: '.swiper-scrollbar',
+      // },
+    });
+  }
 })
-
